7dc807f83d92be2379c6c74d61e378b89a3e8360
InhyeKang <kanginhae0826@gmail.com>
2026-06-07 05:33:29 +0900

    스크롤바 덜컹이는 UI 해결

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -31,10 +31,10 @@ export default function DashboardPage() {
   }
 
   return (
-    <div className="min-h-screen bg-background flex">
+    <div className="h-screen bg-background flex overflow-hidden">
       <Sidebar activeTab={activeTab} setActiveTab={setActiveTab} />
       
-      <div className="flex-1 flex flex-col min-h-screen">
+      <div className="flex-1 flex flex-col">
         <Header onChatOpen={() => setIsChatOpen(true)} />
         
         <main className="flex-1 p-6 overflow-auto">
